4f770d956a1287626fc9a888a24f05473e953dea
BlackTiger684 <BlackTiger684@users.noreply.github.com>
2018-03-02 07:47:15 +0100

    Add files via upload

---
+++ Aufgabe 1/DataSPay.java
@@ -0,0 +1,66 @@
+/*************************************************** 
+Class Results: simple class for Data Science pay
+
+UV Objektorientierte Programmierung / S.Mueller
+***************************************************/
+
+public class DataSPay implements Measurable {
+
+   private String industry; 
+   private int number;
+   private double[] pay;
+	
+	//----------constructors
+	public DataSPay (String industry, int number){
+		this.industry = industry;
+		this.number = number;
+      pay = new double[number];
+	}
+	
+	//----------methods
+	
+	//Interface Measurable
+	public double getMeasure (){
+		return getAveragePay();
+	}
+   
+   public void	setPay(int which, double amount){
+      pay[which] = amount;
+	}
+
+
+	public double getPay(int which){
+		return pay[which];
+	}
+   
+   public void setIndustry(String description){
+      industry = description;
+   }
+   
+   public String getIndustry(){
+      return industry;
+   }
+   
+   public double getTotalPay(){
+      double ges = 0;
+      for(int k = 0; k < pay.length; k++){
+         ges = ges+pay[k];
+      }
+      return ges;
+
+   }
+   
+   public double getAveragePay(int number){
+      double average = 0;
+      average = getTotalPay()/number;
+      return average;
+   }
+   
+   public double getMonthlyAverage(int number){
+      double averagemonth = 0;
+      averagemonth = getTotalPay()/(number*14);
+      return averagemonth;
+   
+   }
+
+}
