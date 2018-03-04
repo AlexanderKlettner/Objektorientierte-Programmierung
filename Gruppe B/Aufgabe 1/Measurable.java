28b795a6f0842466fda365745b65ffae45203d24
Lime91 <36957444+Lime91@users.noreply.github.com>
2018-03-05 00:11:05 +0100

    added static getAverage method (used in demo)

--- Gruppe B/Aufgabe 1/Measurable.java
+++ Gruppe B/Aufgabe 1/Measurable.java
@@ -5,4 +5,25 @@ public interface Measurable {
 
 	//returns the specific measure
 	public double getMeasure ();  
+	
+	
+	
+	//return the average of the data-objects measures
+	//null-references are not counted
+	public static double getAverage (Measurable[] data){
+	
+		double sum = 0.0;
+		int count = 0;
+		for (Measurable m: data)
+			if (m != null){
+				sum = sum + m.getMeasure();
+				count++;
+			}
+		if (count == 0)
+		  return 0.0;
+		else
+			return sum / count;
+	}
 }
+
+
