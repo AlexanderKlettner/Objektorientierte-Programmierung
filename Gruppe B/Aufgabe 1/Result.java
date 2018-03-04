88a11ecc8b62f67718b6e1a9830ff7cba5a4c116
Lime91 <36957444+Lime91@users.noreply.github.com>
2018-03-04 18:17:26 +0100

    Update Result.java
    ein bisschen tuning und Lesbarkeit verbessert

--- Gruppe B/Aufgabe 1/Result.java
+++ Gruppe B/Aufgabe 1/Result.java
@@ -1,50 +1,52 @@
-//fehlt noch
+// group consists of: Steger Marcel, Kurtz Klemens, Thiel Konstantin
+
+
 public class Result implements Measurable {
 	
 	private String name;
-	private int scores;
 	private double[] amount;
+	
 	
 	//constructor for setting the name and the number of scores
 	public Result (String name, int scores) {
 		this.name = name;
-		this.scores = scores;
 		this.amount = new double[scores];
 	}
 
 
 	//How many elements has array?
-	
-	public int arrayInd(){
-		int len = this.amount.length;
-		return len;
+	public int totalScores(){
+		return amount.length;
 	}
 
+	
 	// Interface
 	public double getMeasure() {
 		return getTotalScore();
 	}
 
+	
 	//sets for score which the given amount
 	public void setScore (int which, double amount) {
-		this.amount[which] = amount;
+		
+		if ((which >= 0) && (which < this.amount.length))
+			this.amount[which] = amount;
 	}
 
+	
 	//returns score which
 	public double getScore (int which){
-		// which < this.score, weil score anzahl der Scores ist, aber dann out.ouf.arraybounds waere
-		// da das erste Array element ja mit 0 indiziert ist
-		// Bsp: Scores = 5, dann gibt es ein 5 elementiges Array
-		// a[0], a[1], a[2], a[3] und a[4]
-		// aber in a[5] zu suchen, geht nicht. deshalb fange das ab, falls der User den Score haben will
-		if(which < this.scores) { 
+
+		if((which >= 0) && (which < this.amount.length))
 			return this.amount[which];
-		}
-		else return(0);
+		
+		else return 0;
 	}
 
+	
 	//returns the total score
 	public double getTotalScore (){
+		
 		double totalScore = 0.0;
 		for (double currentScore: amount) {
 			totalScore += currentScore;
@@ -52,20 +54,24 @@ public class Result implements Measurable {
 		return totalScore;
 	}
 
+	
 	//returns a string representation of the name combinded with all
 	// scores and the total score
 	public String toString(){
 		
 		String end = this.name + ": ";
 		
-		for(double currentScore: amount) {
-			end += currentScore + " / ";
+		// we do not want a slash following the last amount. So we better use the standard for-loop instead of the for-each-loop:
+		for(int i = 0; i < amount.length; i++) {
+			end += amount[i];
+			if (i != amount.length-1)
+				end += " / ";
 		}
-		end += " = " + this.getTotalScore();
-		return(end);
 		
-
+		end += " = " + this.getTotalScore();
+		return end;
 	}
 
+	
 }
 
