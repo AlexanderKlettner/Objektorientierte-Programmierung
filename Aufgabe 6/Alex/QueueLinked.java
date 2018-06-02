eeaa92d9e1d8bed991b3671338ae4932da2fb770
AlexanderKlettner <36707814+AlexanderKlettner@users.noreply.github.com>
2018-06-02 16:54:30 +0200

    Update QueueLinked.java

--- Aufgabe 6/Alex/QueueLinked.java
+++ Aufgabe 6/Alex/QueueLinked.java
@@ -1,43 +1,40 @@
 public class QueueLinked <E>{
-   private Node<E> queue;
+   private Node<E> head;
+   private Node<E> tail;
    
    //default constructor for creating an empty queue
    public QueueLinked (){
-      this.queue = null;
+      this.head = null;
+      this.tail = null;
    }
-
-         
+      
    //inserts item at the end of the queue
    public void enqueue(E item){
       Node<E> n = new Node<E>(item);
-      Node <E> copy = queue;
-      
-      if(copy.equals(null)){
-         queue = n;
-      }
-      while(!copy.equals(null)){
-         if(copy.next.equals(null)){
-            copy.next = n;
-         }
-         copy = copy.next;
-      }
-           
+      if(this.size() != 0){
+         this.tail.next = n;
+         this.tail = n;
+      }else { this.head = this.tail = n;}     
    }     
 
    //returns the head of the queue and deletes it from the queue;
    //returns null if queue is empty
    public E dequeue(){
-      Node<E> copy = queue;
-      queue = queue.next;      
-      return copy.data;
+      if(this.size() != 0){
+         Node<E> copy = head;
+         this.head = this.head.next;
+         copy.next = null;
+               
+         return copy.data;
+      }else{return null;}
    }
 
 
    //returns the number of elements in the queue
    public int size(){
       int i = 0;
-      Node<E> copy = queue;
-      while(!copy.equals(null)){
+      Node<E> copy = head;
+      while(copy != null){
          copy = copy.next;
          i++;
       } return i;
@@ -46,7 +43,7 @@ public class QueueLinked <E>{
 
    //test if queue is empty
    public boolean isEmpty(){
-      return queue.equals(null);
+      return head == null;
    }
 
 
@@ -55,10 +52,10 @@ public class QueueLinked <E>{
    //and itemn as the last inserted item
    public String toString (){
       String s = "";
-      Node<E> t = queue;
+      Node<E> t = this.head;
       
-      while(!t.equals(null)){
-         if(!t.next.equals(null)){
+      while(t != null){
+         if(t.next != null){
             s=s+t.data +", ";
          } else { s=s+t.data;}
             t=t.next;
@@ -73,36 +70,50 @@ public class QueueLinked <E>{
    }
 
 
-   private class Node<E> {
+   public class Node<E> {
   
-      private E data;
-      private Node<E> next;
+      public E data;
+      public Node<E> next;
   
       public Node (E d) {
          data = d;
          next = null;
       }
+      public int size(){
+         int s = 0;
+         while(this.next != null){
+            this.next = this.next.next;
+            s++;
+         } return s;
+      }
+      public Node<E> get(int i){
+         Node<E> ret = this.get(1);
+         if(i<this.size()){
+         for(int j = 1; j<=i; j++){
+            ret=this.next;
+         } return ret;
+         }else return null;
+      }
    }
    
    private class IteratorLinked<T> implements MyIterator{
-      private Node<E> head;
+      private Node<E> h;
+      int counter;
       
       IteratorLinked(){
-         this.head = queue;
+         this.h = head;
+         this.counter = -1;
       }
       
       public boolean hasNext() {
-			return this.head != null;
+			return this.counter < this.h.size() - 1;
 		}
 		
-		public E getNext() {
-			E dat = null;
+		public E getNext() {	
 			if(this.hasNext()) {
-				dat = this.head.data;
-				this.head = this.head.next;
-			}
-			return dat;
-			
+            counter++;
+				return h.get(counter).data;
+			}else return null;		
 		}
    }  
 }
